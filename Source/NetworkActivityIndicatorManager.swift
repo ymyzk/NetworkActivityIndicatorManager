dccb0cbdf4ed884a44eb5b5978b19868cc574465
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-04-20 11:29:14 +0900

    Use NSLock instead of GCD

--- Source/NetworkActivityIndicatorManager.swift
+++ Source/NetworkActivityIndicatorManager.swift
@@ -29,50 +29,39 @@ public class NetworkActivityIndicatorManager {
 
     public private(set) var counter = 0
     public private(set) var isNetworkActivityIndicatorVisible = false
-    private let lockQueue = dispatch_queue_create("com.ymyzk.NetworkActivityIndicatorManager.LockQueue", DISPATCH_QUEUE_SERIAL)
-
-    /**
-     Run a block in serial queue for locking
-     */
-    private func runInLockQueue(block: dispatch_block_t) {
-        dispatch_sync(lockQueue, block)
-    }
-
-    /**
-     Run a block in serial queue for locking & update network activity indicator
-     */
-    private func runAndUpdateIndicatorInLockQueue(block: dispatch_block_t) {
-        self.runInLockQueue() {
-            block()
-            self.updateNetworkActivityIndicator()
-        }
-    }
+    private let lock = NSLock()
 
     /**
      Increment counter & update network activity indicator
      */
     @objc public func increment() {
-        runAndUpdateIndicatorInLockQueue() {
-            self.counter += 1
-        }
+        lock.lock()
+        defer { lock.unlock() }
+
+        self.counter += 1
+        self.updateNetworkActivityIndicator()
     }
 
     /**
      Decrement counter & update network activity indicator
      */
     @objc public func decrement() {
-        runAndUpdateIndicatorInLockQueue() {
-            self.counter = max(self.counter - 1, 0)
-        }
+        lock.lock()
+        defer { lock.unlock() }
+
+        self.counter = max(self.counter - 1, 0)
+        self.updateNetworkActivityIndicator()
     }
 
     /**
      Reset counter & update network activity indicator
      */
     public func reset() {
-        runAndUpdateIndicatorInLockQueue() {
-            self.counter = 0
-        }
+        lock.lock()
+        defer { lock.unlock() }
+
+        self.counter = 0
+        self.updateNetworkActivityIndicator()
     }
 
     // MARK: Notifications
@@ -104,8 +93,12 @@ public class NetworkActivityIndicatorManager {
         decrementNotificationNames.remove(name)
     }
 
-    // 必ず lockQueue で実行すること
     private func updateNetworkActivityIndicator() {
+        // Ensure lock is locked
+        guard !lock.tryLock() else {
+            lock.unlock()
+            fatalError("not locked")
+        }
         let isVisible = self.counter > 0
         self.isNetworkActivityIndicatorVisible = isVisible
         dispatch_async(dispatch_get_main_queue()) {
